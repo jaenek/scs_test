a2854e2da28c5994f00a1c7ea27652b2ed924566
jaenek <jaenek@protonmail.com>
2021-11-08 17:45:10 +0100

    Add RESTAssured

--- src/test/java/com/scs_test/AppTest.java
+++ src/test/java/com/scs_test/AppTest.java
@@ -3,6 +3,8 @@ package com.scs_test;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+import io.restassured.RestAssured.*;
+import io.restassured.matcher.RestAssuredMatchers.*;
 
 /**
  * Unit test for simple App.
