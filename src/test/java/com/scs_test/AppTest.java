a738ac357a1f2ef04ceaac38e6d01f33c0230aad
jmaslanka <jmaslanka@protonmail.com>
2021-11-13 15:21:37 +0100

    Add Request/Response logging to file

--- src/test/java/com/scs_test/AppTest.java
+++ src/test/java/com/scs_test/AppTest.java
@@ -1,49 +1,18 @@
 package com.scs_test;
 
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.PrintStream;
 import java.util.HashMap;
 
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExternalResource;
-
 import io.restassured.RestAssured;
-import io.restassured.builder.RequestSpecBuilder;
-import io.restassured.specification.RequestSpecification;
-import io.restassured.filter.log.RequestLoggingFilter;
-import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.response.Response;
 import io.restassured.module.jsv.JsonSchemaValidator;
 
 /**
  * Unit test for Spring Currency Sample App.
  */
 public class AppTest {
-    RequestSpecification requestSpecification;
-
-    @Rule
-    public ExternalResource resource = new ExternalResource() {
-        @Override
-        protected void before() {
-            FileOutputStream fos = null;
-            try {
-                fos = new FileOutputStream("log.txt");
-            } catch (FileNotFoundException ex) {
-                System.out.println("log.txt is a directory!");
-                System.exit(1);
-            }
-            PrintStream log = new PrintStream(fos);
-
-            requestSpecification = new RequestSpecBuilder().setBaseUri("http://localhost:8080")
-                    .addFilter(RequestLoggingFilter.logRequestTo(log))
-                    .addFilter(ResponseLoggingFilter.logResponseTo(log)).build();
-        }
-    };
-
     public void testBasic(String endpoint, String schemaPath) {
-        RestAssured.given().spec(requestSpecification).when().get(endpoint).then().assertThat().statusCode(200).body(JsonSchemaValidator.matchesJsonSchemaInClasspath(schemaPath)).log()
-                .all();
+        RestAssured.given().filter(new RestAssuredRequestFilter()).when().get(endpoint).then().assertThat().body(JsonSchemaValidator.matchesJsonSchemaInClasspath(schemaPath)).extract().response();
     }
 
     @Test
@@ -59,7 +28,7 @@ public class AppTest {
 
     @Test
     public void testMid() {
-        RestAssured.given().spec(requestSpecification).when().queryParam("currency", "usd").get("/mid").then()
-                .assertThat().statusCode(200).body(JsonSchemaValidator.matchesJsonSchemaInClasspath("currency.json")).log().all();
+        RestAssured.given().filter(new RestAssuredRequestFilter()).when().queryParam("currency", "usd").get("/mid").then()
+            .assertThat().body(JsonSchemaValidator.matchesJsonSchemaInClasspath("currency.json"));
     }
 }
